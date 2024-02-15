201db30206f44764c30c75877c3325f1017d2f17
dignityc <dignityc@gmail.com>
2024-02-15 10:09:34 +0000

    minor revision

--- ontochat/chatbot.py
+++ ontochat/chatbot.py
@@ -1,13 +1,14 @@
 from openai import OpenAI
 
 
-client = OpenAI()
-MODEL_NAME = "gpt-3.5-turbo"
-TEMPERATURE = 0
-SEED = 1234
+
 
 
 def chat_completion(messages):
+    client = OpenAI()
+    MODEL_NAME = "gpt-3.5-turbo"
+    TEMPERATURE = 0
+    SEED = 1234
     completion = client.chat.completions.create(
         model=MODEL_NAME,
         messages=messages,
